3a2d6d7b81bba1a5807b40c1a006ca7ba779e9ce
Jacques Grelet <Jacques.grelet@ird.fr>
2017-03-01 15:37:19 +0100

    add degMin2Dec function

--- readCnv.m
+++ readCnv.m
@@ -72,7 +72,7 @@ classdef readCnv < hashtable
             % pre initialization - select filename
             if nargin < 1 || isempty(fileNames)
                 [fileNames, pathName] = uigetfile({'*.cnv','Seabird cnv (*.cnv)'},...
-                    'Select files','MultiSelect','on');                
+                    'Select files','MultiSelect','on');
                 if isempty(fileNames)
                     error(message('MATLAB:reportReader:Empty fileName'));
                 else
@@ -89,7 +89,7 @@ classdef readCnv < hashtable
             % read and extract data from file(s)
             for f = self.fileNames
                 file = char(f);
-                read(self, file);              
+                read(self, file);
             end
             
             
@@ -178,7 +178,7 @@ classdef readCnv < hashtable
                     deg  = str2double(match{1}{1});
                     min  = str2double(match{1}{2});
                     hemi = match{1}{3};
-                    self.Latnum = str2double(DegMin_2_Dec(sprintf('%02d%08.5f', deg, min), hemi));
+                    self.Latnum = str2double(degMinToDec(self, sprintf('%02d%08.5f', deg, min), hemi));
                     continue
                 end
                 
@@ -190,7 +190,7 @@ classdef readCnv < hashtable
                     deg  = str2double(match{1}{1});
                     min  = str2double(match{1}{2});
                     hemi  = match{1}{3};
-                    self.Longnum = str2double(DegMin_2_Dec(sprintf('%03d%08.5f',deg, min), hemi));
+                    self.Longnum = str2double(degMinToDec(self, sprintf('%03d%08.5f',deg, min), hemi));
                     continue
                 end
                 
@@ -284,7 +284,63 @@ classdef readCnv < hashtable
             
         end
         
+        
+        % Converts a GPS latitude or longitude degrees minutes string to a decimal
+        % degrees latitude or longitude
+        %
+        % Returns -1 if called with input argument in wrong format
+        % Returns -2 if not called with input arguments
+        %
+        % Inputs:       degmin - latitude or longitude string to convert
+        %               EWNS - East, West, North, South indicator from NMEA string,
+        %               or use a NULL field ('') if not used
+        % Returns:      dec - decimal degrees version of input
+        % ------------------------------------------------------------------------
+        
+        function dec = degMinToDec(~, degmin, EWNS)
+            % Latitude string format: ddmm.mmmm (dd = degrees)
+            % Longitude string format: dddmm.mmmm (ddd = degrees)
+            
+            if nargin ~= 3
+                dec = '-2';
+            else
+                % Determine  if data is latitude or longitude
+                switch length(strtok(degmin,'.'))
+                    case 4
+                        % latitude data
+                        deg = str2double(degmin(1:2)); % extract degrees portion of latitude string and convert to number
+                        min_start = 3;              % position in string for start of minutes
+                    case 5
+                        % longitude data
+                        deg = str2double(degmin(1:3)); % extract degrees portion of longitude string and convert to number
+                        min_start = 4;              % position in string for start of minutes
+                    otherwise
+                        % data not in correct format
+                        dec = '-1';
+                        return;
+                end
+                
+                minutes = (str2double(degmin(min_start:length(degmin))))/60; % convert minutes to decimal degrees
+                
+                dec = num2str(deg + minutes,'%11.10g'); % degrees as decimal number
+                
+                % add negative sign to decimal degrees if south of equator or west
+                switch EWNS
+                    case 'S'
+                        % south of equator is negative, add -ve sign
+                        dec = strcat('-',dec);
+                    case 'W'
+                        % west of Greenwich meridian is negative, add -ve sign
+                        dec = strcat('-',dec);
+                    otherwise
+                        % do nothing
+                end
+            end
+            
+        end % end of degMinToDec
+        
+        
     end % end of public methods
     
-end
+end % end of class readCnv
 
